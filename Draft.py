582609febf5bf044b3d8d858f8423ff91bf3541c
yurianisimov27 <yurianisimov27@gmail.com>
2020-05-07 13:27:51 +0300

    Added task 4 week 6

--- Draft.py
+++ Draft.py
@@ -1,16 +1,26 @@
-a = list(map(int, input().split()))
-b = []
-for i in range(a[1]):
-    b.append(int(input()))
-b.sort()
-sum = 0
-for i in range(len(b)):
-    sum += b[i]
-    if sum == a[0]:
-        print(i + 1)
-        break
-    if sum > a[0]:
-        print(i)
-        break
-if sum < a[0]:
-    print(len(b))
+n = int(input())
+np = list(map(int, input().split()))
+for i in range(n):
+    np[i] = [np[i], i + 1, 0]
+np.sort()
+m = int(input())
+bu = list(map(int, input().split()))
+for i in range(m):
+    bu[i] = [bu[i], i + 1]
+bu.sort()
+start = 0
+for i in range(n):
+    idx = 0
+    minimum = 10e10
+    for j in range(start, m):
+        tmp = abs(np[i][0] - bu[j][0])
+        if tmp < minimum:
+            idx = j
+            minimum = tmp
+            np[i][2] = bu[j][1]
+        else:
+            break
+    start = idx
+np.sort(key=lambda idx: idx[1])
+for i in range(n):
+    print(np[i][2], end=' ')
