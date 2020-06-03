0fbfbb38b88246151f404d6921f27620d362b3b4
yurianisimov27 <yurianisimov27@gmail.com>
2020-06-03 12:23:22 +0300

    Added task 22 week 3

--- Draft.py
+++ Draft.py
@@ -1,8 +1,3 @@
-A, B = int(input()), int(input())
-while A != B:
-    if A % 2 == 0 and A // 2 >= B:
-        print(':2')
-        A /= 2
-    else:
-        print('-1')
-        A -= 1
+s = input()
+print(s[:s.find('h') + 1], s[s.find('h') + 1:s.rfind('h')].replace('h', 'H'),
+      s[s.rfind('h'):], sep='')
